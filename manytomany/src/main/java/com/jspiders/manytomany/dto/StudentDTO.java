d8345e4f9d22561092e24187b046f2a22f54771d
HP <HP@DESKTOP-U9QQCPP>
2024-03-12 23:52:52 +0530

    commit

---
+++ manytomany/src/main/java/com/jspiders/manytomany/dto/StudentDTO.java
@@ -0,0 +1,34 @@
+package com.jspiders.manytomany.dto;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="student")
+public class StudentDTO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+	@Column(nullable = false)
+private String name;
+	@Column(nullable = false,unique = true)
+private String email;
+	@Column(nullable = false,unique = true)
+private long mobile;
+@ManyToMany
+@Cascade(CascadeType.ALL)
+	private List<CourseDTO> courses;
+}
